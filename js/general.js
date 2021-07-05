2aedaa6e75d472586c4bd50158b75136f2ba3b77
HarshPaliwal <paliwal999harsh+github@gmail.com>
2021-07-05 22:16:21 +0530

    Preloader add

---
+++ js/general.js
@@ -0,0 +1,9 @@
+var x = document.getElementsByClassName("preloader");
+x[0].addEventListener("animationstart",hideContent);
+x[0].addEventListener("animationend",showContent);
+function hideContent(){
+    document.getElementById("bottom-nav").style.visibility=hidden;
+}
+function showContent(){
+    document.getElementById("bottom-nav").style.visibility=visible;
+}
